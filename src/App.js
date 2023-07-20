30f82ebba252a11b80631659829d6e3d164e91dd
ShyloBreanne <sblyall@gmail.com>
2023-07-20 09:37:58 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by <a href="https://www.linkedin.com/in/breannelyall/" target="_blank">Breanne Lyall</a> and is {""}
+        <a href="https://github.com/ShyloBreanne/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
